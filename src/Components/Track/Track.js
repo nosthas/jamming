12bf09d30d36786b2a2430f2a903c35581449d56
Emilio Nosthas <emilio.nosthas@popstudios.com.sv>
2018-01-26 20:51:26 -0600

    first commit

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <a className="Track-action"> - </a>
+    } else {
+      return <a className="Track-action"> + </a>
+    }
+  }
+
+  render() {
+    return(
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
+        </div>
+        {this.renderAction}
+      </div>
+    );
+  }
+}
+
+export default Track;
